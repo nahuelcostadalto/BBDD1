b40a3cd7a5db00b9674b9069bb3dbbb099d653c8
nahuelcostadalto <62125717+nahuelcostadalto@users.noreply.github.com>
2024-11-04 17:15:46 -0300

    Nahuel Costa
    Creo el objeto Alumno , los servicios para agregar alumnos, profesores,clases y estructuras de control.
    Creo la conexión con la base de datos y verifiqué que funcione correctamente, pude hostearlo localmente y hacer un get desde Postman, no estoy pudiendo hacer un Get , pero desde el Visual funciona todo correctamente.
    Faltan agregar funcionalidades.
    PD: controlador_Alumno.py es el main actualmente. Luego se va a separar el codigo que existe en ese archivo.

---
+++ proyecto/main.py
@@ -0,0 +1,20 @@
+#from services.alumnos_service import agregar_alumno
+#from services.clase_service import asignar_clase, agregar_alumno_a_clase
+#from services.login_service import registrar_usuario, autenticacion_de_usuario
+#from datetime import datetime
+#from flask import Flask, jsonify, request
+
+
+# Convertir `fecha_nacimiento` a `datetime.date`
+#fecha_nacimiento = datetime.strptime("2000-01-01", "%Y-%m-%d").date()
+
+
+#try:
+    # Usar `fecha_nacimiento` convertido
+    #agregar_alumno("12346679", "Juan", "Perez", fecha_nacimiento, "099123456", "jufan@correo.com")
+
+#    print("Alumno agregado correctamente")
+
+#except ValueError as e:
+ 
+ #   print(f"Error: {e}")
